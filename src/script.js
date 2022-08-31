7e8b9ae6935ca895d3ee9a405b7255544ae209c9
Mario Esteban Uclés <marioesuc@Marios-MacBook-Pro.local>
2022-08-31 15:47:21 +0200

    feat: throw randomized balls towards screen

--- src/script.js
+++ src/script.js
@@ -3,12 +3,27 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  }  
+
+const SCREEN_DIRECTIONS = {
+    NW: 'NW',
+    N: 'N',
+    NE: 'NE',
+    W: 'W',
+    C: 'C',
+    E: 'E',
+    SW: 'SW',
+    S: 'S',
+    SE: 'SE',
+} 
+
 // Loading
 const textureLoader = new THREE.TextureLoader();
 const normalTexture = textureLoader.load('/textures/NormalMap.png')
-
-// Debug
-const gui = new dat.GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -29,8 +44,12 @@ material.normalMap = normalTexture;
 
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+// const sphere = new THREE.Mesh(geometry,material)
+// const sphere2 = new THREE.Mesh(geometry,material)
+// scene.add(sphere)
+// sphere.direction = 'nw'
+// scene.add(sphere2)
+// sphere.position.z = -100
 
 // Lights
 
@@ -42,50 +61,15 @@ scene.add(pointLight)
 
 // LIGHT 2
 const pointLight2 = new THREE.PointLight(0xff0000, 2)
-// pointLight.position.x = 2
-// pointLight.position.y = 3
-// pointLight.position.z = 4
 pointLight2.position.set(-1.86,1,-1.65 )
 pointLight2.intensity = 1
 scene.add(pointLight2)
 
-// const light1 = gui.addFolder('Light 1')
-
-// light1.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
-// light1.add(pointLight2.position, 'x').min(-6).max(6).step(0.01)
-// light1.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
-// light1.add(pointLight2, 'intensity').min(0).max(10).step(0.01)
-
-// const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1)
-// scene.add(pointLightHelper);
-
 // LIGHT 3
 const pointLight3 = new THREE.PointLight(0xe1ff, 2)
-// pointLight.position.x = 2
-// pointLight.position.y = 3
-// pointLight.position.z = 4
 pointLight3.position.set(2.13,-3,-1.98)
 pointLight3.intensity = 6.8 
 scene.add(pointLight3)
-
-// const light2 = gui.addFolder('Light 2')
-
-// light2.add(pointLight3.position, 'y').min(-3).max(3).step(0.01)
-// light2.add(pointLight3.position, 'x').min(-6).max(6).step(0.01)
-// light2.add(pointLight3.position, 'z').min(-3).max(3).step(0.01)
-// light2.add(pointLight3, 'intensity').min(0).max(10).step(0.01)
-
-// const light2Color = {
-//     color: 0xff0000
-// }
-
-// light2.addColor(light2Color, 'color')
-// .onChange(() => {
-//     pointLight3.color.set(light2Color.color)
-// })
-
-// const pointLightHelper2 = new THREE.PointLightHelper(pointLight3, 1)
-// scene.add(pointLightHelper2);
 
 /**
  * Sizes
@@ -137,7 +121,6 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
-document.addEventListener('mousemove', onDocumentMouseMove)
 
 const updateSphere = (event ) => {
     sphere.position.y = window.scrollY * .001
@@ -145,35 +128,81 @@ const updateSphere = (event ) => {
 
 window.addEventListener('scroll', updateSphere);
 
-let mouseX = 0
-let mouseY = 0
-
-let targetX = 0
-let targetY = 0
-
-const windowX = window.innerWidth /2;
-const windowY = window.innerHeight /2;
-
-function onDocumentMouseMove(event) {
-    mouseX = (event.clientX - windowX)
-    mouseY = (event.clientY - windowY)
-}
-
 const clock = new THREE.Clock()
 
+const generatedSpheres = []
+
+let lastTime = 0
 const tick = () =>
 {
-    targetX = mouseX * .001
-    targetY = mouseY * .001
-
     const elapsedTime = clock.getElapsedTime()
+    const timeDifferenceSeconds = elapsedTime-lastTime
+    if (timeDifferenceSeconds > .2) {
+        const newSphere = new THREE.Mesh(geometry,material)
+        scene.add(newSphere)
+        generatedSpheres.push(newSphere);
+        newSphere.position.z = -50
+        const directionsArray = Object.values(SCREEN_DIRECTIONS)
+        const directionsAmount = Object.values(SCREEN_DIRECTIONS).length
+        const randomDirectionsIndex = getRandomIntInclusive(0, directionsAmount)
+        const randomDirection = directionsArray[randomDirectionsIndex]
+        newSphere.direction = randomDirection
+
+        lastTime = elapsedTime
+    }
+
+    generatedSpheres.forEach( sphere => {
+        if (sphere.position.z > 5) {
+            const index = generatedSpheres.indexOf(sphere);
+            generatedSpheres.splice(index, 1);
+            return
+        }
+        sphere.position.z += .4
+    
+        switch (sphere.direction) {
+            case SCREEN_DIRECTIONS.NW:
+                sphere.position.x -= .005
+                sphere.position.y += .005
+                break;
+            
+            case SCREEN_DIRECTIONS.N:
+                sphere.position.y += .005
+                break;
+
+            case SCREEN_DIRECTIONS.NE:
+                sphere.position.x += .005
+                break;
+
+            case SCREEN_DIRECTIONS.W:
+                sphere.position.x -= .005
+                break;
+
+            case SCREEN_DIRECTIONS.C:
+                break;
+                
+            case SCREEN_DIRECTIONS.E:
+                sphere.position.x += .005
+                break;
+
+            case SCREEN_DIRECTIONS.SW:
+                sphere.position.x -= .005
+                sphere.position.y -= .005
+                break;
+
+            case SCREEN_DIRECTIONS.S:
+                sphere.position.y -= .005
+                break;
+
+            case SCREEN_DIRECTIONS.SE:
+                sphere.position.x += .005
+                sphere.position.y -= .005
+                break;
+        }
+
+    })
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-
-    sphere.rotation.y += .5 * (targetX - sphere.rotation.y)
-    sphere.rotation.x += .05 * (targetY - sphere.rotation.x)
-    sphere.position.z += -.05 * (targetY - sphere.rotation.x)
+    // sphere.rotation.y = .5 * elapsedTime
 
     // Update Orbital Controls
     // controls.update()
